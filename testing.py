43236721383f7d603a391bab8593a2e2e91bdc0a
Sarathkumar <sarathsurpur.93@gmail.com>
2019-01-01 22:53:51 +0800

    Update testing.py

--- testing.py
+++ testing.py
@@ -68,7 +68,7 @@ mycursor.execute("use testdb")
 #inserting rows in batches to the table(repeating until 20k records from here)
 for i in range(1,2000):
     sql = "INSERT INTO demo14 (col1, col2, col3, col4, col5) VALUES (%s, %s,%s,%s,%s);"
-    val = [(i,i*3,i*8,"JURONG"+str(i), "NTU-"+str(i)),((i+10),(i+11),(i+12),"SG-"+str(i), "AG-"+str(i)),(i+21),(i+22),"CV-"+str(i), "MN-"+str(i)),(i+32),(i+31),"FD-"+str(i), "KY-"+str(i))]
+    val = [(i,i*3,i*8,"JURONG"+str(i), "NTU-"+str(i)),((i+10),(i+11),(i+12),"SG-"+str(i), "AG-"+str(i)),((i+20),(i+21),(i+22),"LK-"+str(i), "PL-"+str(i))]
     mycursor.executemany(sql, val)
     
 mydb.commit() 
@@ -139,4 +139,25 @@ myresult = mycursor.fetchall()
 for x in myresult:
   print(x)
 
+ #updating few records in col2
+  
+  mydb = mysql.connector.connect(
+  host="155.69.149.158",
+    port=6000,
+  user="root",
+  passwd="toor",
+  )
+mycursor = mydb.cursor()
+mycursor.execute("use testdb")
+
+mycursor = mydb.cursor()
+
+for i in range(100):
+    a = i*3
+    b = i*5
+    sql = "UPDATE demo14 SET col2 = %s WHERE col2 = %s"
+    inputs = (b, a)
+    mycursor.execute(sql, inputs)
+
+mydb.commit()
 
